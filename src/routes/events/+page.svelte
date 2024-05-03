d58b2701948c1307c0e8310a9966482eab9e4818
JulHjorth <juliehp04@gmail.com>
2024-05-03 13:12:00 +0200

    Added event pages

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<!-- Box around the text -->
+<div class="bg-gray-200 rounded mx-8 py-8 my-8 shadow-md">
+    <!-- Title -->
+    <h1 class="text-3xl font-bold ml-8">Events</h1>
+
+    <!-- Descriptive text -->
+    <p class="ml-9 mt-1">Select the event, you wish to view data on.</p>
+
+</div>
