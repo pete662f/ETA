866f2e62cf5c7acd7c56076c3ad4ac5b613ad8d0
Peter <peter.kaae.thorhauge@gmail.com>
2024-05-09 15:00:39 +0200

    added server side signin

--- src/routes/signout/+page.server.ts
+++ src/routes/signout/+page.server.ts
@@ -1,4 +1,4 @@
 // Nedded by authjs for server-side using Form Actions
-import { signIn } from "../../auth"
+import { signOut } from "../../auth"
 import type { Actions } from "./$types"
-export const actions: Actions = { default: signIn }
+export const actions: Actions = { default: signOut }
