ed500cd408a3366a397739188cf1a3f6144ca30a
JulHjorth <juliehp04@gmail.com>
2024-05-15 14:18:50 +0200

    margin changed

--- src/routes/[uuid]/economy/+page.svelte
+++ src/routes/[uuid]/economy/+page.svelte
@@ -4,16 +4,19 @@
     export let data: PageData;
 </script>
 
-<div class="bg-gray-200 rounded mx-8 py-8 my-8 shadow-md">
-    <!-- Title -->
-    <h1 class="text-3xl font-bold ml-8">Economy</h1>
+<div class="bg-gray-200 rounded mx-8 py-6 my-8 shadow-md">
+    <div class="mx-8">
 
-    <!-- Descriptive text -->
-    <p class="ml-9 mt-1">Money!</p>
-
-    <!-- Dropdown select menu -->
-    <div class="relative inline-block text-left ml-8 mt-6">
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <!-- Title -->
+        <h1 class="text-3xl font-bold">Economy</h1>
+        
+        <!-- Descriptive text -->
+        <p class="mt-1">Money!</p>
+        
+        <!-- Dropdown select menu -->
+        <div class="relative inline-block text-left mt-6">
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            </div>
         </div>
     </div>
 </div>
