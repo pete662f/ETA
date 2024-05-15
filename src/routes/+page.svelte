f9eddd376af8903f641efa43e5744ae01e2b46e8
JulHjorth <juliehp04@gmail.com>
2024-05-15 09:46:33 +0200

    login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,20 @@
     
 </script>
 
-<div class="container mx-auto">
-    <h1>Home</h1>
-    <SignIn>
+<!-- Box, thats around the hole text -->
+<div class="bg-gray-200 rounded mx-8 py-8 my-8 shadow-md">
+    
+    <!-- Title for the side -->
+    <h1 class="text-3xl font-bold ml-8">Welcome to Event Tickets Analytics</h1>
 
-    </SignIn>
+    <!-- Describing text -->
+    <p class="ml-9 mt-1">We are here to help you, sort your data. blablabla</p>
+
+    <!-- Login part -->
+    <div class="flex ml-9 mt-7">
+        <div class="bg-green-100 border border-gray-300 hover:border-gray-500 font-bold py-1 px-2 rounded-md">
+            <SignIn>     
+            </SignIn>
+        </div>
+    </div>
 </div>
